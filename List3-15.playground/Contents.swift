acccfda85ba400b48ade468e95d6d92224fe2978
midori004 <midori253@gmail.com>
2017-04-07 09:17:15 +0900

    modify List3-15

--- List3-15.playground/Contents.swift
+++ List3-15.playground/Contents.swift
@@ -17,3 +17,17 @@ print(a.mL)
 a.ounce += 8.0
 print(a.ounce)
 print(a.mL)
+
+var b = Ounce(ounce: 2.0)
+var c = Ounce(ounce: 10.0)
+print("\(b.mL)mL, \(c.mL)mL")
+swap(&b, &c)
+print("\(b.mL)mL, \(c.mL)mL")
+swap(&b, &c)
+print("\(b.mL)mL, \(c.mL)mL")
+swap(&b.ounce, &c.ounce)
+print("\(b.ounce)oz, \(c.ounce)oz")
+
+var z: Double = 0.1
+swap(&b.ounce, &z)
+print("\(b.ounce)oz, \(z)")
